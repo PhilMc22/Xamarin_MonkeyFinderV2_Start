e01ddec9a7a246e99b1d667b5e74b4dbbbce522e
PhilMc22 <pmc2292@gmail.com>
2020-04-30 09:05:53 +0100

    Implemented Step 5 (BaseViewModel)

--- Start/MonkeyFinder/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Start/MonkeyFinder/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -5,8 +5,39 @@ using Xamarin.Forms;
 
 namespace MonkeyFinder.ViewModel
 {
-    public class BaseViewModel 
+    public class BaseViewModel : INotifyPropertyChanged
     {
-       
+        bool isBusy;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if (isBusy == value)
+                    return;
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        string title;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title == value)
+                    return;
+                title = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsNotBusy));
+            }
+        }
+
+        public bool IsNotBusy => !IsBusy;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
